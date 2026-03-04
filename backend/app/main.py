ca0a3d1301c743fb523080fabb31e2f2e7dd5df5
pedrocavalcantip <pfcc@cin.ufpe.br>
2026-03-04 00:55:55 -0300

    feat: adicionando middleware de cors para comunicacao com frontend

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from app import models
 from app.database import engine
 from app.routes import router
+from fastapi.middleware.cors import CORSMiddleware
 
 models.Base.metadata.create_all(bind=engine)
 
@@ -12,6 +13,13 @@ app = FastAPI(
 
 app.include_router(router)
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def root():
